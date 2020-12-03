e5ceda943da77b523cddcf441ea03e92a0b2ce8c
MarkSant23 <markosantek55@gmail.com>
2020-12-03 19:41:43 +0100

    Updated delete

---
+++ mmaCrudPin/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//(($) => {
+//     Index = () => {
+//        var $this = this;
+//         initialize = ()=> {
+
+//            $(".popup").on('click',(e) => {
+//                modelPopup(this);
+//            });
+
+//             modelPopup =(reff)=> {
+//                var url = $(reff).data('url');
+
+//                $.get(url).done((data) => {
+//                    debugger;
+//                    $('#modal-create-edit-user').find(".modal-dialog").html(data);
+//                    $('#modal-create-edit-user > .modal', data).modal("show");
+//                });
+
+//            }
+//        }
+
+//        $this.init = () => {
+//            initialize();
+//        };
+//    }
+//    $(() => {
+//        var self = new Index();
+//        self.init();
+//    });
+//}(jQuery));  
